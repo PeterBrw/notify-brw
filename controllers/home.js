4854596eb19cd1010e02d75ee086ead587ec2a54
PeterBrw <peter.giurgiu@gmail.com>
2021-08-31 01:04:04 +0300

    mail

--- controllers/home.js
+++ controllers/home.js
@@ -22,7 +22,7 @@ exports.sendMail = (req, res, next) => {
     const {name, company, email, phone, message} = req.body
     const msg = {
         to: email, // Change to your recipient
-        from: 'info@brwxyz.com', 
+        from: 'peter.giurgiu@gmail.com', 
         subject: 'Sending with SendGrid is Fun',
         text: 'and easy to do anywhere, even with Node.js',
         html: `<h1>${message}</h1>`,
